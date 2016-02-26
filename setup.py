bb99e48d89a463687680bd0a1414dd0e09407c23
Mayu Laierlence <minacle@live.com>
2016-02-26 11:56:54 +0900

    phase4

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
@@ -9,15 +11,15 @@ with open(path.join(root, "README.rst")) as f:
 
 setup(
     name="yarh",
-    version="0.3", #phase3
+    version="0.3.1",  # phase4
     description="Yet Another Rough HTML",
     long_description=readme,
     url="https://github.com/minacle/yarh",
     author="Mayu Laierlence",
     author_email="minacle@live.com",
     license="BSD",
-    classifiers=[ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 2 - Pre-Alpha",
+    classifiers=[  # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
