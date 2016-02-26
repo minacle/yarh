575908c836889a8918099c9802778fd5a4df8828
Mayu Laierlence <minacle@live.com>
2016-02-26 11:47:28 +0900

    wait, what?

--- setup.py
+++ setup.py
@@ -4,32 +4,32 @@ from os import path
 
 root = path.abspath(path.dirname(__file__))
 
-with open(path.join(root, 'README.rst')) as f:
+with open(path.join(root, "README.rst")) as f:
     readme = f.read()
 
 setup(
-    name='yarh',
-    version='0.3', #phase3
-    description='Yet Another Rough HTML',
+    name="yarh",
+    version="0.3", #phase3
+    description="Yet Another Rough HTML",
     long_description=readme,
-    url='https://github.com/minacle/yarh',
-    author='Mayu Laierlence',
-    author_email='minacle@live.com',
-    license='BSD',
+    url="https://github.com/minacle/yarh",
+    author="Mayu Laierlence",
+    author_email="minacle@live.com",
+    license="BSD",
     classifiers=[ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
-    keywords='yarh html',
+    keywords="yarh html",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'yarh=yarh.__main__:main',
+        "console_scripts": [
+            "yarh=yarh.__main__:main",
         ],
     },
 )
